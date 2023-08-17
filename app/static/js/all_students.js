8e0c7b3f31e03fdda5a7e74c64bf8c8775c04f0a
pankajcaini <61980327+pankajcaini@users.noreply.github.com>
2023-08-17 13:35:49 +0530

    Update all_students.js
    corrected record spelling

--- app/static/js/all_students.js
+++ app/static/js/all_students.js
@@ -6,7 +6,7 @@ students.forEach(student_delete_link => {
     student_delete_link.addEventListener('click', function(e){
         e.preventDefault();
         const href = e.target.href;
-        var yes_link = document.getElementById('yes-delete-recird-link');
+        var yes_link = document.getElementById('yes-delete-record-link');
         yes_link.href = href;
         var modal = document.getElementById('modal');
         modal.classList.add('display-modal');
